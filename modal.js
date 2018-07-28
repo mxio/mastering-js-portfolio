ed9c89a3492d02303fef4a21351ddc8a548a9491
mxio <mxio@users.noreply.github.com>
2018-07-28 14:19:56 -0500

    Added closing of modal functionality and toggled search input placeholder text

--- modal.js
+++ modal.js
@@ -1,14 +1,43 @@
 (function(window) {
-	//var
+	//variables for opening
 	var modal = document.querySelector('.modal');
 	var modalContainer = document.querySelector('.modal-container');
 	var form = document.getElementById('search-form');
 
-	//add event listener
+	//variable for closing
+	var closeModal = document.getElementById('close');
+
+	//variables for search input placeholder 
+	var searchInput = document.getElementById('search-form-input');
+	var placeholder = document.querySelector('.placeholder');
+
+	//add event listener to display modal
 	form.addEventListener('submit', function (event) {
 		event.preventDefault();
 		modal.classList.add("clicked"); 
 		modalContainer.classList.add("display");
 	}, false);
 
+	//add event listener to close modal
+	closeModal.addEventListener('click', closeTheModel);
+
+	//function to close modal
+	function closeTheModel(event) {
+		modal.classList.remove("clicked");
+		modalContainer.classList.remove("display");
+	} 
+
+	//add event listener to the search input field
+	searchInput.addEventListener('input', activatePlaceholder);
+
+	//function to keep move placeholder text
+	function activatePlaceholder() {
+		if (searchInput.value) {
+			placeholder.classList.add('placeholder-active');
+		}
+		else {
+			placeholder.classList.remove('placeholder-active');
+		}
+	}
+	
 })(window);
